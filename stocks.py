60ca86e4ca48b2757dda59f3a2c01258f671d03c
Brian James <bjames647@gmail.com>
2014-12-22 01:19:08 -0600

    working on formatting the string to get an int out of it

--- stocks.py
+++ stocks.py
@@ -1,28 +1,46 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as BS
 import urllib.request
 import time
-#from pdb import set_trace as debug
+from pdb import set_trace as debug
 
 def doRequest(request):
 	result = urllib.request.urlopen(request).read()
-	soup = BeautifulSoup(result).prettify().encode('UTF-8')
+	soup = BS(result, from_encoding='UTF-8')
 	return soup
 
 def getRequest():
 	#request = input('Website: ')
-	soup = doRequest('http://www.allpennystocks.com/aps_us/hot_nasdaq_stocks.asp')
+	soup = 'http://www.allpennystocks.com/aps_us/hot_nasdaq_stocks.asp'
 	return soup
 
-
+def doMining(soup):
+	data = []
+	#
+	i = 0
+	soup = soup.findAll("td", {"width":"10%"})
+	for blarg in soup:
+		data.append(str(blarg.contents))
+		
+		#print(i)
+	#debug()
+	counter = 0
+	for d in data:
+		print(i)
+		balls = data[counter]
+		#print(balls)
+		i = i + balls
+		counter = counter + 1
+	#print(data[0])
 
 def main():
 	while True:
-		soup = getRequest()
-		
-		print(soup)
+		request = getRequest()
+		soup = doRequest(request)
+		#debug()
+		doMining(soup)
+		#print(soup)
 		inputVar = input('To run again enter Y. Selection: ')
 		if inputVar != 'y' and inputVar != 'Y':
 			break
 
 main()
-#debug()
